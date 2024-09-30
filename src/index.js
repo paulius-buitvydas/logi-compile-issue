87f1a0ed4ed8e2f7ddba40aed3e9a00bbb86a6bb
Paulius Buitvydas <paulius.buitvydas@juvare.com>
2024-09-30 18:09:52 +0300

    initial

--- src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const logi_embed_1 = require("logi-embed");
-// This code is do demonstrate typescript compile issue
-(0, logi_embed_1.initEmbedManager)({ getToken: () => Promise.reject() }, '');
